ad3ab2869c541aadb041a2e02cfabd35e86f4d42
avnercaleb <avnercaleb@hotmail.com>
2024-01-31 19:57:58 -0300

    Criação do Projeto

---
+++ midas-card-manager/ms-service-discovery/src/main/java/servicediscovery/MsServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsServiceDiscoveryApplication.class, args);
+	}
+
+}
