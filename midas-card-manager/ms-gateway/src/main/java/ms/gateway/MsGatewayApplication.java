244b36f59e2fa1ded319d5b85ee2fdaf407a6361
avnercaleb <avnercaleb@hotmail.com>
2024-02-01 13:29:53 -0300

    Implementação do Cloud Gateway

---
+++ midas-card-manager/ms-gateway/src/main/java/ms/gateway/MsGatewayApplication.java
@@ -0,0 +1,25 @@
+package ms.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator router(RouteLocatorBuilder builder) {
+		return builder
+			.routes()
+			.route(r -> r.path("/clientes/**").uri("lb://ms-clientes"))
+			.build();
+	}
+}
