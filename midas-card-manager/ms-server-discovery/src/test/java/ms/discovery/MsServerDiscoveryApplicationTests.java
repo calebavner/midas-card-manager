163f147b09e4f0d474f1771659f64bc84bea149b
avnercaleb <avnercaleb@hotmail.com>
2024-02-01 06:47:52 -0300

    Criação do projeto, server dicovery e ms de clientes

---
+++ midas-card-manager/ms-server-discovery/src/test/java/ms/discovery/MsServerDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServerDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
